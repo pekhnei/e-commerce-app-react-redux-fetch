30ce6a15bba3add6b25fc8a1abc0a4e9766070f4
David Pekhnei <david.pekhnei@gmail.com>
2022-08-25 13:02:30 +0300

    init commit

---
+++ src/component/Cart.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import product from "./Product";
+import {useSelector} from "react-redux";
+
+const Cart = () => {
+    return (
+        <div>
+            <div className="row">
+                <div className="col-md-4">
+                    <img src={product.image} alt={product.title} height="200px" width="180px"/>
+                </div>
+                <div className="col-md-4">
+                    <h3>{product.title}</h3>
+                    <p className="lead fw-bold">
+                        {product.qty} X ${product.price} = $
+                        {product.qty * product.price}
+                    </p>
+                    <button className="btn btn-outline-dark me-4" onClick=''>
+                        <i className="fa fa-minus"></i>
+                    </button>
+                    <button className="btn btn-outline-dark" onClick='/*!TODO*/'>
+                        <i className="fa fa-plus"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Cart;
